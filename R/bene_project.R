5948e57a9daa585dadfdff2aebf80b5c15f116f6
Benedikt <benediktclaus@msn.com>
2020-08-28 12:31:53 +0200

    Update personal project temaplet
    Correct library name and creation of first section

--- R/bene_project.R
+++ R/bene_project.R
@@ -26,7 +26,8 @@ bene_project <- function(path, ...) {
 
   # Document Preambles
   cleaning_preamble <- paste(c("library(tidyverse)", "library(janitor)", "library(haven)"), collapse = "\n")
-  analyses_preamble <- paste(c("library(tidyverse)", "library(benelib)", "library(extrafonts)", "", "theme_set(theme_bene())"), collapse = "\n")
+  analyses_preamble <- paste(c("library(tidyverse)", "library(benelib)", "library(extrafont)", "", "theme_set(theme_bene())",
+                               "", "", "# Data Import -------------------------------------------------------------"), collapse = "\n")
 
   writeLines(cleaning_preamble, con = file(file.path(path, "03 R", "data-cleaning.R")))
   writeLines(analyses_preamble, con = file(file.path(path, "03 R", "analyses.R")))
