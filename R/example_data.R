059204db46cdb12fdbfdeb4126e737c7b6f6fa37
Benedikt <benediktclaus@msn.com>
2021-01-08 11:28:13 +0100

    The same

---
+++ R/example_data.R
@@ -0,0 +1,20 @@
+#' Palmer Penguin Dataset
+#'
+#' A cleaned version of the Palmer Penguins dataset from the package
+#' `palmerpenguins` (i.e. all missing removed). This serves primarily as an
+#' example dataset for plotting examples.
+#'
+#' @format A tibble with 333 rows and 8 columns
+#' \describe{
+#'   \item{species}{Penguin's species}
+#'   \item{island}{Penguin's island}
+#'   \item{bill_length_mm}{Bill length in mm}
+#'   \item{bill_depth_mm}{Bill depth in mm}
+#'   \item{flipper_length_mm}{Flipper length in mm}
+#'   \item{body_mass_g}{Weight in g}
+#'   \item{sex}{Sex}
+#'   \item{year}{Year of observation}
+#' }
+#'
+#' @source \url{https://github.com/allisonhorst/palmerpenguins}
+"palmer_penguins"
