98a89a9e7780e07596f9647aa2bbfd923b6b93f8
Benedikt Claus <benediktclaus@msn.com>
2021-11-03 13:28:21 +0100

    Add use_analysis_template function

---
+++ R/use_templates.R
@@ -0,0 +1,18 @@
+#' Use Analysis Template
+#'
+#' Execute this function to use a default raw analysis template. By default, the
+#' template will be placed into folder `03 R`, but the destination can be
+#' changed via `destination_path`
+#'
+#' @param destination_path Folder to where the template should be copied. Defaults to "03 R"
+#'
+#' @importFrom fs path path_wd path_package file_copy
+#'
+#' @return
+#' @export
+use_analysis_template <- function(destination_path) {
+  destination_path <- path(path_wd(), "03 R")
+  template_path <- path(path_package("benelib"), "templates", "analysis.R")
+
+  file_copy(template_path, destination_path)
+}
