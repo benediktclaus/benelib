4c2c6366989caf558a712d097f7aed60b146632d
Benedikt Claus <benediktclaus@msn.com>
2021-11-03 13:16:54 +0100

    Rename template folder

---
+++ inst/templates/analyses.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(benelib)
+
+theme_set(theme_bene())
+use_personal_theme()
+
+
+
+
+# Data Import -------------------------------------------------------------
+
+
+
+
+# Analyses ----------------------------------------------------------------
---
+++ inst/templates/data-cleaning-excel.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(readxl)
+library(janitor)
+library(labelled)
+
+
+
+
+# Data Import -------------------------------------------------------------
